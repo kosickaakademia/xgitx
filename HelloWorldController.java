50d387fea4952618cf9dd3e0d9a454b447dc3dab
kosickaakademia <roland.onofrej@kosickaakademia.sk>
2021-02-18 12:24:35 +0100

    My first commit

---
+++ HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.in28minutes.restdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController
+{
+    @GetMapping( path = "/hello-world")
+    public String helloWorld(){
+        return "hello World";
+    }
+}
